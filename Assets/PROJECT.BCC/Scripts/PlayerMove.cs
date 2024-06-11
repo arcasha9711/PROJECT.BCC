38f585f92714c5d53c27f5d6c1d6e6d9a91e8660
ParkShinYoung <crib7398@gmail.com>
2024-06-12 04:00:20 +0900

    Cinemachine QuaterView 만들었음

---
+++ Assets/PROJECT.BCC/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace BCC
+{
+    public class PlayerMove : MonoBehaviour
+    {
+     
+        private void Update()
+        {
+            if(Input.GetKey(KeyCode.Mouse1))
+            {
+               
+            }
+        }
+
+    }
+}
