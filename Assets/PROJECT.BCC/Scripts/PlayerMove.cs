95fb3356560510d0f3ad7486d6c0bf50c9919d1e
ParkShinYoung <crib7398@gmail.com>
2024-06-12 04:26:03 +0900

    Starter Asset Import NavMesh add

--- Assets/PROJECT.BCC/Scripts/PlayerMove.cs
+++ Assets/PROJECT.BCC/Scripts/PlayerMove.cs
@@ -8,12 +8,23 @@ namespace BCC
 {
     public class PlayerMove : MonoBehaviour
     {
-     
+        NavMeshAgent agent;
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
         private void Update()
         {
             if(Input.GetKey(KeyCode.Mouse1))
             {
-               
+               Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if(Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    agent.SetDestination(hit.point);
+                }
             }
         }
 
