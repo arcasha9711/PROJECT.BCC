f14dcb7734d1c174e0ef35c1d756635767356585
ParkShinYoung <crib7398@gmail.com>
2024-06-12 14:46:28 +0900

    드레그로 셀렉트, 클릭한곳으로 이동 설정

---
+++ Assets/PROJECT.BCC/Scripts/Selection System/ISelectable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCU
+{
+public interface ISelectable
+    {
+        public GameObject Selection {  get; }
+        public void Select();
+
+        public void Deselect();
+
+    }
+}
