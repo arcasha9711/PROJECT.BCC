84b4fe3c7b4649f5ccfed44c2885ce213deade0d
ParkShinYoung <crib7398@gmail.com>
2024-06-19 02:16:10 +0900

    Animation 복구

--- Assets/PROJECT.BCC/Scripts/Selection System/SelectableChracter.cs
+++ Assets/PROJECT.BCC/Scripts/Selection System/SelectableChracter.cs
@@ -11,12 +11,14 @@ namespace SCU
 
         public GameObject Selection => selectStateUI;
         public NavMeshAgent navMeshAgent;
+        public Animator animator;
 
         public GameObject selectStateUI;
 
         private void Awake()
         {
             SpawnedCharacters.Add(this);
+            animator = GetComponentInChildren<Animator>();
         }
 
         private void OnDestroy()
@@ -37,6 +39,20 @@ namespace SCU
         public void SetDestination(Vector3 targetDestination)
         {
             navMeshAgent.SetDestination(targetDestination);
+        }
+        private void Update()
+        {
+            if (navMeshAgent.velocity != Vector3.zero)
+            {
+                Vector3 localVelocity = transform.InverseTransformDirection(navMeshAgent.velocity);
+                animator.SetFloat("Horizontal", localVelocity.x);
+                animator.SetFloat("Vertical", localVelocity.z);
+            }
+            else
+            {
+                animator.SetFloat("Horizontal", 0);
+                animator.SetFloat("Vertical", 0);
+            }
         }
     }
 }
