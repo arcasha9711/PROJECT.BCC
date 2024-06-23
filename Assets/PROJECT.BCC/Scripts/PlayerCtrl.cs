2404d1a0e53415a0d6587e24e87a867200402931
ParkShinYoung(Mac) <crib7398@gmail.com>
2024-06-23 20:03:19 +0900

    움직임 속도 Inspector창에서 변경 가능

--- Assets/PROJECT.BCC/Scripts/PlayerCtrl.cs
+++ Assets/PROJECT.BCC/Scripts/PlayerCtrl.cs
@@ -8,20 +8,15 @@ namespace BCC
     public class PlayerCtrl : MonoBehaviour
     {
         public float movespeed = 3.0f;
-        public float sprintSpeed = 5.0f;
-        public float speedChangeRate = 10.0f;
-
 
         private void Update()
         {
-           GroupMove();
-
-            
+            GroupMove();
         }
 
         void GroupMove()
         {
-            transform.Translate(Vector3.forward * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.forward * movespeed * Time.deltaTime, Space.World);
         }
     }
 }
