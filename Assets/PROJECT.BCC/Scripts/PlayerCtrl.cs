065b9867b8b4e517acaba0d37a0c834199fc45f3
ParkShinYoung <crib7398@gmail.com>
2024-06-24 16:26:45 +0900

    no message

--- Assets/PROJECT.BCC/Scripts/PlayerCtrl.cs
+++ Assets/PROJECT.BCC/Scripts/PlayerCtrl.cs
@@ -8,74 +8,35 @@ namespace BCC
 {
     public class PlayerCtrl : MonoBehaviour
     {
-        public float moveSpeed = 3.0f;
-        public float sprintSpeed = 5.0f;
-        public float speedChangeRate = 10.0f;
+        //public NavMeshAgent navMeshAgent;
+        //public Animator animator;
 
-        private Animator animator;
-        private NavMeshAgent navMeshAgent;
-        private float currentSpeed = 0.0f;
-
-        private void Start()
+        private void Awake()
         {
-            animator = GetComponent<Animator>();
-            navMeshAgent = GetComponent<NavMeshAgent>();
-
-            if (navMeshAgent != null)
-            {
-                navMeshAgent.speed = moveSpeed;
-            }
+            //animator = GetComponentInChildren<Animator>();
         }
+
+        public float movespeed = 3.0f;
 
         private void Update()
         {
-            Move();
-            UpdateAnimator();
-        }
-
-        void Move()
-        {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-
-            Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
-
-            if (direction.magnitude >= 0.1f)
+            /*if (navMeshAgent.velocity != Vector3.zero)
             {
-                float targetSpeed = moveSpeed;
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    targetSpeed = sprintSpeed;
-                }
-
-                currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * speedChangeRate);
-                navMeshAgent.speed = currentSpeed;
-
-                Vector3 moveDirection = direction * currentSpeed;
-                navMeshAgent.Move(moveDirection * Time.deltaTime);
-
-                // Rotate player to face the movement direction
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0.0f, targetAngle, 0.0f);
+                Vector3 localVelocity = transform.InverseTransformDirection(navMeshAgent.velocity);
+                animator.SetFloat("Horizontal", localVelocity.x);
+                animator.SetFloat("Vertical", localVelocity.z);
             }
             else
             {
-                currentSpeed = Mathf.Lerp(currentSpeed, 0.0f, Time.deltaTime * speedChangeRate);
-                navMeshAgent.speed = currentSpeed;
-            }
+                animator.SetFloat("Horizontal", 0);
+                animator.SetFloat("Vertical", 0);
+            }*/
+            
         }
 
-        void UpdateAnimator()
+        public void GroupMove()
         {
-            if (animator != null)
-            {
-                // Use magnitude of the input vector as speed
-                float speed = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).magnitude;
-                animator.SetFloat("Speed", speed);
-
-                // Optionally, update other parameters such as Direction if needed
-                animator.SetFloat("Direction", Input.GetAxis("Horizontal"));
-            }
+            transform.Translate(Vector3.forward * movespeed * Time.deltaTime, Space.World);
         }
     }
 }
