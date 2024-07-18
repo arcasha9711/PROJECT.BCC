e0625039e5c16dc5931278d65373368f3b990a0b
ParkShinYoung <crib7398@gmail.com>
2024-07-19 01:28:31 +0900

    no message

--- Assets/PROJECT.BCC/Scripts/Weapon/Bullet.cs
+++ Assets/PROJECT.BCC/Scripts/Weapon/Bullet.cs
@@ -8,21 +8,30 @@ namespace BCC
     {
         public int damage;
         public float lifeTime = 3f;
+        public float maxDistance = 50f;
+
+        private Vector3 startPosition;
         
 
         private void Start()
         {
+            startPosition = transform.position;
             Destroy(gameObject, lifeTime);
         }
+
+        private void Update()
+        {
+            float traveledDistance = Vector3.Distance(startPosition, transform.position);
+            if(traveledDistance >= maxDistance)
+            {
+                Destroy(gameObject);
+            }
+        }
+
 
         private void OnCollisionEnter(Collision collision)
         {
             Destroy(gameObject);
-        }
-
-        private void Exception()
-        {
-            
         }
     }
 }
